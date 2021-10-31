59e010bd159e85a2e0ce1cb7654402193e6b94f1
getfounder <getfounder@yandex.ru>
2021-10-31 19:45:11 +0400

    save

--- main.py
+++ main.py
@@ -3,14 +3,7 @@ import config as cfg
 from classes import *
 from functions import *
 
-import os
-
 from time import sleep
-
-import keyboard
-
-# Горячая клавиша для остановки при помощи вызова ошибки
-keyboard.add_hotkey("ctrl+alt+p", lambda: os.__exit__)
 
 # Инициализация окна и отображение его поверх остальных приложений
 emulator_window = Window()
